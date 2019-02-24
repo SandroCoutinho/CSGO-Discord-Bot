a574f166f402145c23b29da2f45d940038439ec6
Sandro Coutinho <capslock_donater@hotmail.com>
2019-02-24 09:53:04 +0000

    [TASK] Removed keys

--- src/main/java/com/sandroc/discord/csgobot/utils/DefaultSetting.java
+++ src/main/java/com/sandroc/discord/csgobot/utils/DefaultSetting.java
@@ -2,9 +2,9 @@ package com.sandroc.discord.csgobot.utils;
 
 public enum DefaultSetting {
     BOT_ENABLED("botEnabled", true),
-    BOT_TOKEN("botToken", "NTQ3NTIxOTAyODY1MDIzMDAw.D1P2hA.K_RJC9EB8nq7KVILN-yuA44qvxA"),
+    BOT_TOKEN("botToken", "BOT_TOKEN"),
     BOT_OWNERID("botOwnerId", "466822971856650260"),
-    STEAM_API_KEY("steamAPIKey", "A7FD55100FEFCA6FA07908BA34029851"),
+    STEAM_API_KEY("steamAPIKey", "STEAM_API_KEY"),
     BOT_COMMAND_PREFIX("botCommandPrefix", "!");
 
     private String key;
